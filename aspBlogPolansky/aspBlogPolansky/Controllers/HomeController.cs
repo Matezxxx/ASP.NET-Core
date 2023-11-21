155852010ddaaf4922de34bc60741023e802719c
Matezxxx <Polansky.mat@seznam.cz>
2023-11-21 19:36:39 +0100

    ok

---
+++ aspBlogPolansky/aspBlogPolansky/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using aspBlogPolansky.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace aspBlogPolansky.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+      
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult References()
+        {
+            return View();
+        }
+
+        public IActionResult Skills()
+        {
+            return View();
+        }
+    }
+}
---
+++ aspBlogPolansky/aspBlogPolansky/UserRoles.cs
@@ -0,0 +1,7 @@
+﻿namespace aspBlogPolansky
+{
+    public static class UserRoles
+    {
+        public const string Admin = "Admin";
+    }
+}
